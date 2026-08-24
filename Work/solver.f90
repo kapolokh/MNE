b70a90af2b6e6570b5ee5e0b3f8d053a219b1aa8
Kirill Polokhalo <kapolokh@ncsu.edu>
2026-08-23 21:34:31 -0400

    Incorporated multigroup. Created power iterations. Future work: redo reflective sweep

--- Work/solver.f90
+++ Work/solver.f90
@@ -1,24 +1,44 @@
 program solver_n
     implicit none
     integer :: out
-    integer :: I,J,K,M,BCL,BCR,BCB,BCT
-    real, allocatable :: dx(:),dy(:),mu(:),eta(:),w(:),SigmaT(:),Sigma1_2(:),source(:,:)
+    integer :: I,J,K,M,G,BCL,BCR,BCB,BCT
+    real, allocatable :: dx(:),dy(:),mu(:),eta(:),w(:),SigmaT(:,:),SigmaA(:,:),SigmaS(:,:),nuSigmaF(:,:),Sigma1_2(:,:),phi(:,:,:)
     integer,allocatable :: material(:,:)
     integer :: count0, count1, rate
+    real :: keff
     real :: diff
-    real :: tol
-    integer :: maxiter
+    real :: tol, tolk
+    integer :: maxiter,maxouter
+    integer :: ii,jj,gg
+
 
     call system_clock(count=count0)
     call system_clock(count_rate = rate)
 
-    open(newunit=out, file="Sn_bwr_Output", status="replace", action = "write")
+    open(newunit=out, file="Final", status="replace", action = "write")
     call Version_data(out)
-    call Input_data(out,I,J,K,M,BCL,BCR,BCB,BCT,dx,dy,mu,eta,w,SigmaT,Sigma1_2,material,source,tol,maxiter)
-    call Input_check(out,I,J,K,M,dx,dy,mu,eta,w,SigmaT,Sigma1_2,BCL,BCR,BCB,BCT,material,source,tol,maxiter)
-    call Input_echo(out,I,J,K,mu,eta,w,BCL,BCR,BCB,BCT,dx,dy,SigmaT,Sigma1_2,source,material)
-    call Transport_solver(out,I,J,K,M,dx,dy,mu,eta,w,SigmaT,Sigma1_2,material,source,tol,maxiter)
-    
+    call Input_data(out,I,J,K,M,G,BCL,BCR,BCB,BCT,dx,dy,mu,eta,w,SigmaT,SigmaA,SigmaS,nuSigmaF,Sigma1_2,material,tol,maxiter)
+    call Input_check(out,I,J,K,M,G,dx,dy,mu,eta,w,SigmaT,SigmaA,SigmaS,BCL,BCR,BCB,BCT,material,tol,maxiter)
+    call Input_echo(out,I,J,K,G,mu,eta,w,BCL,BCR,BCB,BCT,dx,dy,SigmaT,SigmaS,SigmaA,nuSigmaF,Sigma1_2,material)
+
+    allocate(phi(I,J,G))
+
+    call Power_iteration(out,I,J,K,M,G,dx,dy,mu,eta,w,SigmaT,SigmaA,Sigma1_2,nuSigmaF,material,tol,maxiter,tolk,maxouter,phi,keff)
+
+    write(out,'(/,a)') "Discrete Ordinates Method Solution"
+    do gg=1,G
+        write(out,'(/,a,i0)') "Group ", gg
+        write(out,'(a3,1x,a3,1x,a26)') "i","j","Cell-Averaged Scalar Flux"
+        do jj = 1,J
+            do ii = 1,I
+                write(out,'(i3,1x,i3,1x,es16.8)') ii,jj,phi(ii,jj,gg)
+            end do
+        end do
+    end do
+write(out,'(/,a,f12.6)') "k-effective = ", keff
+
+
+
     call system_clock(count=count1)
     diff = real(count1-count0)/real(rate)
     write(out,'(/,a,f10.4)') "Run Time (s):", diff
@@ -40,17 +60,17 @@ program solver_n
     end subroutine Version_data
 
 
-    subroutine Input_data(out,I,J,K,M,BCL,BCR,BCB,BCT,dx,dy,mu,eta,w,SigmaT,Sigma1_2,material,source,tol,maxiter)
+    subroutine Input_data(out,I,J,K,M,G,BCL,BCR,BCB,BCT,dx,dy,mu,eta,w,SigmaT,SigmaA,SigmaS,nuSigmaF,Sigma1_2,material,tol,maxiter)
         implicit none
         integer, intent(in) :: out
-        integer, intent(out) :: I,J,K,M
+        integer, intent(out) :: I,J,K,M,G
         integer,intent(out) :: BCL,BCR,BCB,BCT
-        real, allocatable, intent(out) :: dx(:),dy(:),mu(:),eta(:),w(:),SigmaT(:),Sigma1_2(:)
+        real, allocatable, intent(out) :: dx(:),dy(:),mu(:),eta(:),w(:),SigmaT(:,:), & 
+        SigmaA(:,:),SigmaS(:,:),nuSigmaF(:,:),Sigma1_2(:,:)
         integer,allocatable,intent(out) :: material(:,:)
-        real,allocatable,intent(out) :: source(:,:)   !Source is a decimal value
         real, intent(out) :: tol
         integer, intent(out) :: maxiter
-        integer :: n,ii,jj
+        integer :: n,ii,jj,gg
 
         !Reading size of mesh and preallocating the matrix 
         read(*,*) I,J
@@ -66,41 +86,47 @@ program solver_n
             read(*,*) mu(n),eta(n),w(n)
         enddo
 
-        !Reading # of materials
+        !Reading # of materials & groups
+        read(*,*) G
         read(*,*) M 
-        allocate(SigmaT(M),Sigma1_2(M))   !We created the array for each X/S of length of materials, and then we fill it out in the order that our input is positioned in (T followed by S)
+        allocate(SigmaT(M,G),SigmaA(M,G),nuSigmaF(M,G),Sigma1_2(M,G))   !We created the array for each X/S of length of materials, and then we fill it out in the order that our input is positioned in (T followed by S)
         do n=1,M 
-            read(*,*) SigmaT(n),Sigma1_2(n)
+            do gg=1,G
+                read(*,*) SigmaT(n,gg),SigmaA(n,gg),nuSigmaF(n,gg),Sigma1_2(n,gg)
+            end do
+        end do
+
+        !Create/Calculate in-group scattering cross-section
+        allocate(SigmaS(M,G))
+        do n=1,M 
+            do gg=1,G
+                SigmaS(n,gg)= SigmaT(n,gg)-SigmaA(n,gg)
+            end do
         end do
 
         !Reading BCs
         read(*,*) BCL,BCR,BCB,BCT
         
-
         !Create material matrix
-        allocate(material(I,J),source(I,J))
+        allocate(material(I,J))
         do jj=1,J
             read(*,*) (material(ii,jj), ii=1,I)    !Could not do a nested loop, therefore did this trick since read function only reads horizontally all characters on the line.
         end do
 
-        !Creating source matrix
-        do jj=1,J
-            read(*,*) (source(ii,jj), ii=1,I)        
-        end do
-
         !Read convergence criteria
         read(*,*) tol,maxiter
+        read(*,*) tolk,maxouter
 
         write(out,'(a)') "Input has been read, variables assigned, matrices created"
 
     end subroutine Input_data
 
-    subroutine Input_check(out,I,J,K,M,dx,dy,mu,eta,w,SigmaT,Sigma1_2,BCL,BCR,BCB,BCT,material,source,tol,maxiter)
+    subroutine Input_check(out,I,J,K,M,G,dx,dy,mu,eta,w,SigmaT,SigmaA,SigmaS,BCL,BCR,BCB,BCT,material,tol,maxiter)
         implicit none
-        integer :: n,ii,jj
-        real,intent(in) :: dx(:),dy(:),mu(:),eta(:),w(:),SigmaT(:),Sigma1_2(:), source(:,:)
+        integer :: n,ii,jj,gg
+        real,intent(in) :: dx(:),dy(:),mu(:),eta(:),w(:),SigmaT(:,:),SigmaA(:,:),SigmaS(:,:)
         integer,intent(in) :: BCL,BCR,BCB,BCT, material(:,:)
-        integer, intent(in) :: out,I,J,K,M
+        integer, intent(in) :: out,I,J,K,M,G
         real,intent(in) :: tol
         integer,intent(in) :: maxiter
         
@@ -115,6 +141,10 @@ program solver_n
         end if
         if (M < 1) then 
              write(out,'(a,i0,a,f12.6)') "ERROR: # of materials has to be >= 1"
+            stop 1
+        end if
+        if (G < 1) then 
+             write(out,'(a,i0,a,f12.6)') "ERROR: # of energy groups has to be >= 1"
             stop 1
         end if
         do jj=1,J 
@@ -144,18 +174,24 @@ program solver_n
 
         !X/S Check
         do n=1,M 
-            if(SigmaT(n) < 0.0) then
-            write(out,'(a,i0,a,f12.6)') "ERROR: Total X/S less than 0"
-            stop 3
-            end if
-            if(Sigma1_2(n) < 0.0) then
-            write(out,'(a,i0,a,f12.6)') "ERROR: Scattering X/S less than 0"
-            stop 3
-            end if
-            if (Sigma1_2(n) > SigmaT(n)) then
-            write(out,'(a,i0,a,f12.6)') "ERROR: Scattering X/S greater than Total X/S"
-            stop 3
-            end if
+            do gg=1,G
+                if(SigmaT(n,gg) < 0.0) then
+                write(out,'(a,i0,a,f12.6)') "ERROR: Total X/S less than 0"
+                stop 3
+                end if
+                if(SigmaA(n,gg) < 0.0) then
+                write(out,'(a,i0,a,f12.6)') "ERROR: Absorption X/S less than 0"
+                stop 3
+                end if
+                if(SigmaS(n,gg) < 0.0) then
+                write(out,'(a,i0,a,f12.6)') "ERROR: Scattering X/S less than 0"
+                stop 3
+                end if
+                if (SigmaS(n,gg) > SigmaT(n,gg)) then
+                write(out,'(a,i0,a,f12.6)') "ERROR: Scattering X/S greater than Total X/S"
+                stop 3
+                end if
+            end do
         end do
 
         !BC Check
@@ -182,10 +218,6 @@ program solver_n
                     write(out,'(a,i0,a,f12.6)') "ERROR: Material Type non-existant "
                     stop 5
                 end if
-                if (source(ii,jj) < 0.0) then
-                    write(out,'(a,i0,a,f12.6)') "ERROR: Source value < 0 "
-                    stop 6
-                end if
             end do
         end do
         
@@ -195,20 +227,20 @@ program solver_n
         end if
 
         if (maxiter < 1 ) then
-            write(out,'(a)') "Error: maximum # of iterations ahs to be grater than 1"
+            write(out,'(a)') "Error: maximum # of iterations has to be grater than 1"
             stop 8
         end if
 
     end subroutine Input_check
 
-    subroutine Input_echo(out,I,J,K,mu,eta,w,BCL,BCR,BCB,BCT,dx,dy,SigmaT,Sigma1_2,source,material)
+    subroutine Input_echo(out,I,J,K,G,mu,eta,w,BCL,BCR,BCB,BCT,dx,dy,SigmaT,SigmaS,SigmaA,nuSigmaF,Sigma1_2,material)
         implicit none
-        integer, intent(in) :: out,I,J,K
+        integer, intent(in) :: out,I,J,K,G
         integer, intent(in) :: BCL,BCR,BCB,BCT
-        real, intent(in) :: mu(:),eta(:),w(:),dx(:),dy(:),source(:,:),SigmaT(:),Sigma1_2(:)
+        real, intent(in) :: mu(:),eta(:),w(:),dx(:),dy(:),SigmaT(:,:),SigmaS(:,:),SigmaA(:,:),nuSigmaF(:,:),Sigma1_2(:,:)
         integer,intent(in) :: material(:,:)
-        integer :: n,ii,jj,m
-        !We print out some of the necessary input variavles to output file
+        integer :: n,ii,jj,m,gg
+        !We print out some of the necessary input variables to output file
         !Write the Discrete Ordinates Line
         write(out,'(/,a)') "Discrete Ordinates/Octant:"
         write(out,'(a3,1x,a10,1x,a10,1x,a10)') "n", "mu", "eta", "w"
@@ -223,33 +255,114 @@ program solver_n
 
         !Write the cell data line
         write(out,'(/,a)') "Computational Cell Data:"
-        write(out,'(a3,1x,a3,1x,a8,1x,a10,1x,a10,1x,a10,1x,a10,1x,a10)') &
-         "i", "j", "Material", "dx", "dy","SigmaT","Sigma1_2","Source"
-        do jj = 1,J 
-            do ii = 1,I 
+        write(out,'(a3,1x,a3,1x,a8,1x,a5,1x,a10,1x,a10,1x,a10,1x,a10,1x,a10,1x,a10,1x,a10)') &
+        "i", "j", "Material", "Group", "dx", "dy", "SigmaT", "SigmaA", "SigmaS", "nuSigmaF", "Sigma1_2"
+        do jj = 1,J
+            do ii = 1,I
                 m = material(ii,jj)
-                write(out,'(i3,1x,i3,1x,i8,1x,f10.5,1x,f10.5,1x,f10.5,1x,f10.5,1x,f10.5)') &
-                 ii,jj,m,dx(ii),dy(jj),SigmaT(m),Sigma1_2(m),source(ii,jj)
+                    do gg = 1,G
+                        write(out,'(i3,1x,i3,1x,i8,1x,i5,1x,f10.5,1x,f10.5,1x,f10.5,1x,f10.5,1x,f10.5,1x,f10.5,1x,f10.5)') &
+                        ii,jj,m,gg,dx(ii),dy(jj),SigmaT(m,gg),SigmaA(m,gg),SigmaS(m,gg),nuSigmaF(m,gg),Sigma1_2(m,gg)
+                    end do
             end do
         end do
 
     end subroutine Input_echo
 
-    subroutine Transport_solver(out,I,J,K,M,dx,dy,mu,eta,w,SigmaT,Sigma1_2,material,source,tol,maxiter)
-        implicit none
-        integer,intent(in) :: out
-        integer, intent(in) :: I,J,K,M,maxiter
-        real, intent(in) :: dx(:), dy(:), mu(:), eta(:), w(:), SigmaT(:), Sigma1_2(:), source(:,:), tol
-        integer, intent(in) :: material(:,:)
-        real, allocatable :: phi (:,:)
-        allocate(phi(I,J))
-        write(out,'(/a)') "will solve D.O. here"
 
-        call inner(out,I,J,K,M,dx,dy,mu,eta,w,SigmaT,Sigma1_2,material,source,maxiter,tol,phi)
+    subroutine Power_iteration(out,I,J,K,M,G,dx,dy,mu,eta,w,SigmaT,SigmaA,Sigma1_2,nuSigmaF, &
+                            material,tol,maxiter,tolk,maxouter,phi,keff)
+    implicit none
+    integer, intent(in) :: out,I,J,K,M,G,maxiter,maxouter
+    real, intent(in) :: dx(:),dy(:),mu(:),eta(:),w(:)
+    real, intent(in) :: SigmaT(:,:),SigmaA(:,:),Sigma1_2(:,:),nuSigmaF(:,:)
+    integer, intent(in) :: material(:,:)
+    real, intent(in) :: tol,tolk
+    real, intent(inout) :: phi(:,:,:)
+    real, intent(out) :: keff
+    real, allocatable :: SigmaS_self(:,:), q(:,:), Fsrc(:,:), chi(:)
+    real :: Fsum_old, Fsum_new, diffk
+    integer :: ii,jj,mm,gg,outer
 
-    end subroutine Transport_solver
+    allocate(q(I,J),Fsrc(I,J),SigmaS_self(M,G),chi(G))
 
-    subroutine inner(out,I,J,K,M,dx,dy,mu,eta,w,SigmaT,Sigma1_2,material,source,maxiter,tol,phi)
+    !Self-scatter only (total out-scatter minus what leaves to the next group)
+    do mm=1,M
+        do gg=1,G
+            SigmaS_self(mm,gg) = SigmaT(mm,gg) - SigmaA(mm,gg) - Sigma1_2(mm,gg)
+        end do
+    end do
+
+    !Fission spectrum - fast fission only, no thermal fission neutrons born
+    chi = 0.0
+    chi(1) = 1.0
+
+    phi = 1.0
+    keff = 1.0
+
+    do outer = 1,maxouter
+
+        !Fission source from the current flux guess: F(i,j) = sum_g' nuSigmaF(g')*phi(g')
+        Fsrc = 0.0
+        do jj=1,J
+            do ii=1,I
+                mm = material(ii,jj)
+                do gg=1,G
+                    Fsrc(ii,jj) = Fsrc(ii,jj) + nuSigmaF(mm,gg)*phi(ii,jj,gg)
+                end do
+            end do
+        end do
+        Fsum_old = sum(Fsrc)
+
+        !Solve groups in sequence (no upscatter, so group g only needs group g-1's
+        !already-updated flux, plus the fission source built above)
+        do gg=1,G
+            do jj=1,J
+                do ii=1,I
+                    mm = material(ii,jj)
+                    q(ii,jj) = chi(gg)*Fsrc(ii,jj)/keff
+                    if (gg > 1) then
+                        q(ii,jj) = q(ii,jj) + Sigma1_2(mm,gg-1)*phi(ii,jj,gg-1)
+                    end if
+                end do
+            end do
+            call inner(out,I,J,K,M,dx,dy,mu,eta,w,SigmaT(:,gg),SigmaS_self(:,gg),material,q, &
+                       maxiter,tol,phi(:,:,gg),.false.)
+        end do
+
+        !Updated fission source and k
+        Fsrc = 0.0
+        do jj=1,J
+            do ii=1,I
+                mm = material(ii,jj)
+                do gg=1,G
+                    Fsrc(ii,jj) = Fsrc(ii,jj) + nuSigmaF(mm,gg)*phi(ii,jj,gg)
+                end do
+            end do
+        end do
+        Fsum_new = sum(Fsrc)
+
+        keff = keff * (Fsum_new/Fsum_old)
+        diffk = abs(Fsum_new/Fsum_old - 1.0)
+
+        write(out,'(a,i5,a,f12.6,a,es12.5)') "Outer iteration ", outer, "  k = ", keff, "  diff = ", diffk
+
+        if (diffk <= tolk) then
+            write(out,'(/,a)') "Power iteration converged successfully."
+            write(out,'(a,i6)') "Outer iterations consumed:", outer
+            exit
+        end if
+
+        if (outer == maxouter) then
+            write(out,'(/,a)') "Power iteration terminated unsuccessfully."
+        end if
+
+    end do
+
+end subroutine Power_iteration
+
+
+    subroutine inner(out,I,J,K,M,dx,dy,mu,eta,w,SigmaT,SigmaS,material,source,maxiter,tol,phi,verbose)
         implicit none
         !Input:
         !I,J - number of spatial cells
@@ -259,7 +372,7 @@ program solver_n
         !mu,eta - ordiantes
         !w - weight
         !SigmaT - total X/S
-        !Sigma1_2 - scattering X/S
+        !SigmaS - total scattering X/S
         !material - material map
         !source - fixed source in each material cell
         !tol - convergence criterion
@@ -269,10 +382,11 @@ program solver_n
 
         integer,intent(in) :: I,J,K,M,out
         real, intent(in) :: dx(:),dy(:),mu(:),eta(:),w(:)
-        real, intent(in) :: SigmaT(:),Sigma1_2(:),source(:,:)
+        real, intent(in) :: SigmaT(:),SigmaS(:),source(:,:)
         integer, intent(in) :: material(:,:)
         integer, intent(in) :: maxiter
         real, intent(in) :: tol
+        logical, intent(in) :: verbose
         real, intent(out) :: phi(:,:)
         real,allocatable :: phi_old(:,:), q(:,:)
         real :: diff,maxdiff
@@ -287,7 +401,7 @@ program solver_n
             do jj = 1,J
                 do ii = 1,I
                     mm = material(ii,jj)
-                    q(ii,jj) = source(ii,jj) + Sigma1_2(mm)*phi_old(ii,jj)
+                    q(ii,jj) = source(ii,jj) + SigmaS(mm)*phi_old(ii,jj)
                 end do
             end do
 
@@ -309,30 +423,37 @@ program solver_n
             end do
 
             if (maxdiff <= tol) then
-                write(out,'(/,a)') "Inner iterations converged successfully."
-                write(out,'(a,i6)') "Iterations consumed:", it
-                write(out,'(a,1x,es12.5)') "Convergence criterion achieved:", maxdiff
+                if (verbose) then
+                    write(out,'(/,a)') "Inner iterations converged successfully."
+                    write(out,'(a,i6)') "Iterations consumed:", it
+                    write(out,'(a,1x,es12.5)') "Convergence criterion achieved:", maxdiff
+                end if
                 exit
             end if
 
             if (it == maxiter) then
-                write(out,'(/,a)') "Inner iterations terminated unsuccessfully."
-                write(out,'(a,i6)') "Iterations consumed:", it
-                write(out,'(a,1x,es12.5)') "Convergence criterion achieved:", maxdiff
+                if (verbose) then
+                    write(out,'(/,a)') "Inner iterations terminated unsuccessfully."
+                    write(out,'(a,i6)') "Iterations consumed:", it
+                    write(out,'(a,1x,es12.5)') "Convergence criterion achieved:", maxdiff
+                end if
                 exit
             end if
 
             phi_old = phi
         end do
 
-        write(out,'(/,a)') "Discrete Ordinates Method Solution"
-        write(out,'(a3,1x,a3,1x,a26)') "i","j","Cell-Averaged Scalar Flux"
+        if (verbose) then
 
-        do jj = 1,J
-            do ii = 1,I
-                write(out,'(i3,1x,i3,1x,es16.8)') ii,jj,phi(ii,jj)
+            write(out,'(/,a)') "Discrete Ordinates Method Solution"
+            write(out,'(a3,1x,a3,1x,a26)') "i","j","Cell-Averaged Scalar Flux"
+
+            do jj = 1,J
+                do ii = 1,I
+                    write(out,'(i3,1x,i3,1x,es16.8)') ii,jj,phi(ii,jj)
+                end do
             end do
-        end do
+        end if
         
     end subroutine inner 
 
